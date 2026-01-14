42abc1fbb2f018d8751a7a0d0079a9dbb0c6fc08
Chevuri Santhosh <santh@Santhosh-MacBook-Pro-2.local>
2026-01-14 14:36:03 +0530

    Hello

---
+++ slack_home.js
@@ -0,0 +1,6 @@
+let lis=document.getElementById("features-btn")
+lis.addEventListener("mouseover",A)
+function A(){
+//   document.getElementById("features").scrollIntoView({behavior:"smooth"});
+    
+};
